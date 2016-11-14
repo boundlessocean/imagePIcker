d6a60b9ee10827d654a4e8f24b7812b7c478a451
boundlessocean <boundlessocean@gongxiangcloud.com>
2016-11-14 10:37:09 +0800

    使用方法： 	//初始化方法二     _imagePickerController = [[JHImagePickerController alloc]initWithIsCaches:YES andIdentifier:@"abc"];
        _imagePickerController.delegate = self;
        //若不需要裁剪图片，则设置该属性。
        _imagePickerController.isEditImage = NO;
    - (void)selectImageClicked{
        UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"选取图片来源" message:@"" preferredStyle:UIAlertControllerStyleAlert];
        UIAlertAction * cameraAction = [UIAlertAction actionWithTitle:@"相机" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
            [_imagePickerController selectImageFromCameraSuccess:^(UIImagePickerController *imagePickerController) {
                [self presentViewController:imagePickerController animated:YES completion:nil];
            } fail:^{
                NSLog(@"无法获取相机权限");
            }];
        }];
        UIAlertAction * photoAction = [UIAlertAction actionWithTitle:@"图片" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
            [_imagePickerController selectImageFromAlbumSuccess:^(UIImagePickerController *imagePickerController) {
                [self presentViewController:imagePickerController animated:YES completion:nil];
            } fail:^{
                NSLog(@"无法获取相簿权限");
            }];
        }];
        UIAlertAction * cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
        }];
        [alert addAction:cameraAction];
        [alert addAction:photoAction];
        [alert addAction:cancelAction];
        [self presentViewController:alert animated:YES completion:nil];
    }
    
    - (void)selectImageFinishedAndCaches:(UIImage *)image cachesIdentifier:(NSString *)identifier isCachesSuccess:(BOOL)isCaches{
        _imageView.image = image;
    }
    
    - (void)readImageClicked{
        UIImage * image = [_imagePickerController readImageFromCachesIdentifier:@"abc"];
        if (image) {
            _imageView.image = image;
        }else {
            _imageView.image = nil;
            NSLog(@"read image fail");
        }
    }
    
    - (void)deleteImageClicked{
        if ([_imagePickerController removeCachePictures]){
            _imageView.image = nil;
            NSLog(@"remove pics success");
        }else {
            NSLog(@"remove fail");
        }
    }

---
+++ BLImagePickerController.m
@@ -0,0 +1,186 @@
+//
+//  BLImagePickerController.m
+//  EaseUIImagePickerController
+//
+//  Created by boundlessOcean on 16/6/13.
+//
+//
+
+#import "BLImagePickerController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <Photos/Photos.h>
+#import <PhotosUI/PhotosUI.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface BLImagePickerController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
+@property (nonatomic,strong) UIImagePickerController * imagePickerController;
+
+@property (nonatomic,strong) UIImage * image;
+
+@end
+
+//是否采用裁剪后的图片
+static BOOL bl_isEditImage = YES;
+
+@implementation BLImagePickerController
+
+- (void)setIsEditImage:(BOOL)isEditImage{
+    bl_isEditImage = isEditImage;
+}
+
+#pragma mark - 初始化方法
+- (instancetype)initWithIsCaches:(BOOL)isCaches andIdentifier:(NSString *)identifier{
+    self = [super init];
+    if (self) {
+        self.isCaches = isCaches;
+        self.identifier = identifier;
+    }
+    return self;
+}
+
+#pragma mark - 来自相机
+- (void)selectImageFromCameraSuccess:(CameraSuccess)success fail:(CameraFail)failure{
+    if (failure) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        if (status == AVAuthorizationStatusRestricted || status == AVAuthorizationStatusDenied) {
+            failure(self.imagePickerController);
+            return;
+        }
+    }
+    if (success) {
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+            self.imagePickerController.mediaTypes = @[(NSString *)kUTTypeImage];
+            self.imagePickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+            success(self.imagePickerController);
+        }
+    }
+}
+
+#pragma mark - 来自相簿
+- (void)selectImageFromAlbumSuccess:(AlbumSuccess)success fail:(AlbumFail)failure{
+    if (failure) {
+        ALAuthorizationStatus status = [ALAssetsLibrary authorizationStatus];
+        if (status == ALAuthorizationStatusRestricted || status == ALAuthorizationStatusDenied) {
+            failure(self.imagePickerController);
+            return;
+        }
+    }
+    if (success) {
+        self.imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        success(self.imagePickerController);
+    }
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    NSString * mediaType = info[UIImagePickerControllerMediaType];
+    if (mediaType == (NSString *)kUTTypeImage) {
+        if (bl_isEditImage) {
+            self.image = info[UIImagePickerControllerEditedImage];
+        }else {
+            self.image = info[UIImagePickerControllerOriginalImage];
+        }
+        NSString * url = info[UIImagePickerControllerMediaURL];
+        NSLog(@"uuu:%@",url);
+        if ([self.delegate respondsToSelector:@selector(selectImageFinishedAndCaches:cachesIdentifier:isCachesSuccess:)]) {
+            BOOL cachesStatus = [self saveImageToCaches:self.image
+                                             identifier:self.identifier];
+            [self.delegate selectImageFinishedAndCaches:self.image
+                                       cachesIdentifier:self.identifier
+                                        isCachesSuccess:cachesStatus];
+            [_imagePickerController dismissViewControllerAnimated:YES completion:nil];
+        }
+    }
+}
+
+#pragma mark - 读取缓存的图片
+- (UIImage *)readImageFromCachesIdentifier:(NSString *)identifier {
+    NSString * path = [NSString stringWithFormat:@"%@/%@",cachesPath(),identifier];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        NSData * imgeData = [[NSFileManager defaultManager] contentsAtPath:path];
+        if (imgeData) {
+            UIImage * image = [[UIImage alloc]initWithData:imgeData];
+            return image;
+        }
+    }
+    return nil;
+}
+
+#pragma mark - 删除指定缓存的图片
+- (BOOL)removeCachePictureForIdentifier:(NSString *)identifier {
+    NSString * path = [NSString stringWithFormat:@"%@/%@",cachesPath(),identifier];
+    if ([[NSFileManager defaultManager]fileExistsAtPath:path]) {
+        NSError * error ;
+        [[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+        if (error) {
+            NSLog(@"remove picture for id:%@ failure",identifier);
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+#pragma mark - 删除全部图片
+- (BOOL)removeCachePictures{
+    if ([[NSFileManager defaultManager]fileExistsAtPath:cachesPath()]) {
+        NSError * error;
+        [[NSFileManager defaultManager] removeItemAtPath:cachesPath() error:&error];
+        if (error) {
+            NSLog(@"remove pictures fail , error : %@ , path = %@",error,cachesPath());
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+#pragma mark - 缓存图片
+- (BOOL)saveImageToCaches:(UIImage *)image identifier:(NSString *)identifier {
+    NSData * imageData = UIImageJPEGRepresentation(image, 0.5);
+    if (imageData) {
+        if (![[NSFileManager defaultManager] fileExistsAtPath:cachesPath()]) {
+            NSError * error = nil;
+            [[NSFileManager defaultManager] createDirectoryAtPath:cachesPath()
+                                      withIntermediateDirectories:YES
+                                                       attributes:nil
+                                                            error:&error];
+            if (error) {
+                NSLog(@"create cache dir error: %@   path: %@",error,cachesPath());
+                return false;
+            }
+            NSLog(@"creat cache dir success :%@",cachesPath());
+        }
+        if (self.identifier) {
+            NSString * path = [NSString stringWithFormat:@"%@/%@",cachesPath(),self.identifier];
+            BOOL isSuccess = [[NSFileManager defaultManager] createFileAtPath:path
+                                                                     contents:imageData
+                                                                   attributes:nil];
+            if (isSuccess) {
+                return YES;
+            }
+        }
+    }
+    return false;
+}
+
+- (UIImagePickerController *)imagePickerController{
+    if (_imagePickerController == nil) {
+        _imagePickerController = [[UIImagePickerController alloc]init];
+        _imagePickerController.delegate = self;
+        _imagePickerController.allowsEditing = bl_isEditImage;
+    }
+    return _imagePickerController;
+}
+
+static inline NSString * cachesPath(){
+    return [NSString stringWithFormat:@"%@/blImageCaches",NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, true)[0]];
+}
+
+- (void)dealloc{
+    NSLog(@"dealloc : %@",self);
+}
+@end
